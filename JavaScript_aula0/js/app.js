57a1ba521663633fccb8558a331960d3a3140e64
julia <juju.a.andrioti@gmail.com>
2026-09-23 15:24:37 -0300

    commit

---
+++ JavaScript_aula0/js/app.js
@@ -0,0 +1,43 @@
+// Comentário da linha 1 
+
+/*
+Comentário de várias linhas 
+*/
+
+/*
+alert -Interação de pop-up com no usuário
+alert("Bem-vindo ao meu Site");
+*/ 
+
+
+/*
+ Utilizando as ferramentas de desenvolvimento 
+console.log("Seja Bem-vindo");
+*/
+
+// Utilizando Prompt
+
+/*
+
+let nome = prompt("Qual o seu nome?")
+
+console.log(nome)
+
+*/
+
+
+/*
+let nomeAluno = prompt("Qual o seu nome?")
+let nota1 = prompt("Insira a nota 1")
+let nota2 = prompt("Insira a nota 2")
+let nota3 = prompt("Insira a nota 3")
+let media = (nota1 + nota2 + nota3) / 3 
+
+console.log( "O nome do aluno é: " + nomeAluno + " . A média do aluno é: " + media)
+
+*/
+
+let num1 = Number(prompt("Insira o número 1"))
+let num2 = Number(prompt("Insira o número 2"))
+let soma = num1 + num2
+console.log(soma)
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ProgramacaoWeb1
+# 
